45de92fb835eb09c8b56f5140cbe4623596c4a2d
Fayimora Femi-Balogun <fayimora@hotmail.co.uk>
2011-07-03 11:03:25 +0100

    changed dbTester to try and connect to my website's page

--- src/dbtesterQmul.java
+++ src/dbtesterQmul.java
@@ -8,16 +8,17 @@ public class dbtesterQmul{
     public static void main(String[] args) {
     System.out.println("MySQL Connect Example.");
     Connection conn = null;
-    String url = "jdbc:mysql://dbprojects.eecs.qmul.ac.uk/";
-    String dbName = "fbfb3";
+    String url = "jdbc:mysql://fayimoracom2.ipagemysql.com/";
+    String dbName = "mywebsite1";
     String driver = "com.mysql.jdbc.Driver";
-    String userName = "fbfb3"; 
-    String password = "RPbqkgmSq2VRX";
+    String userName = "fayimoracom2"; 
+    String password = "fayimora100%";
+    
     try
     {
       //Load the database driver
       Class.forName(driver).newInstance();
-      //Get a connection to the dtabase
+      //Get a connection to the database
       System.out.println("bout to connect");
       conn = DriverManager.getConnection(url+dbName,userName,password);
       System.out.println("Connected to the database");
@@ -36,7 +37,7 @@ public class dbtesterQmul{
       Statement stmt = conn.createStatement() ;
 
       // Execute the query
-      ResultSet rs = stmt.executeQuery( "SELECT * FROM blog_users" ) ;
+      ResultSet rs = stmt.executeQuery( "SELECT * FROM blog_posts ;" ) ;
 
       // Loop through the result set
       while( rs.next() )
