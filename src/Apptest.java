aaf81a58b0ee3fda36cb87135cbd7c59c5989d3a
Wenebunha Etitinwo <weniswow15@yahoo.com>
2011-07-19 22:54:03 +0100

    Added EtisWene to copyright info :D

--- src/Apptest.java
+++ src/Apptest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005-2010 Substance Fayimora Femi-Balogun. All Rights Reserved.
+ * Copyright (c) 2005-2010 Substance Fayimora Femi-Balogun and EtisWene. All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
